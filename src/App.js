b70745cd562055f3638113b6fa36fa7c3c417a41
nekonomicus <87386132+nekonomicus@users.noreply.github.com>
2025-03-18 12:09:33 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HipMeasurementInterface from './components/HipMeasurementInterface';
+
+function App() {
+  return (
+    <div className="App">
+      <HipMeasurementInterface />
+    </div>
+  );
+}
+
+export default App;
